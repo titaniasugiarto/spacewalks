271fd86dc8fa1960827933d9721fbe029e984345
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2025-02-04 12:09:38 +0100

    Implement informative file names

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,7 +1,7 @@
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('/home/sarah/Projects/ssi-ukrn-fair-course/data.json', 'r')
-data_t = open('/home/sarah/Projects/ssi-ukrn-fair-course/data.csv','w')
-g_file = 'myplot.png'
+data_f = open('./eva-data.json', 'r')
+data_t = open('./eva-data.csv','w')
+g_file = './cumulative_ev_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
