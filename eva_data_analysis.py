b75f1a224fef896f263523d94338620aad08fd24
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2025-02-05 14:44:57 +0100

    Commenting the purpose of the code blocks

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -6,13 +6,16 @@ input_file = open('./eva-data.json', 'r', encoding='utf-8')
 output_file = open('./eva-data.csv', 'w', encoding='utf-8')
 graph_file = './cumulative_eva_graph.png'
 
-eva_df = pd.read_json(input_file, convert_dates=['date'])
+# Create a DataFrame from the JSON file
+eva_df = pd.read_json(input_file, convert_dates=['date'])  
 eva_df['eva'] = eva_df['eva'].astype(float)
 eva_df.dropna(axis=0, inplace=True)
 eva_df.sort_values('date', inplace=True)
 
+# Save the DataFrame to a CSV file
 eva_df.to_csv(output_file, index=False)
 
+# Plot the cumulative time spent in space to date
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
