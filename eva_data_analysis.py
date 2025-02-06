76f6d7f1afd87604d18f40c8ad3b16842f6ed784
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2025-02-06 15:23:53 +0100

    Docstrings updated, specified type

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -10,7 +10,7 @@ def read_json_to_dataframe(input_file):
     Clean the data by removing any incomplete rows and sort by date
 
     Args:
-        input_file_ (str): The path to the JSON file.
+        input_file (str): The path to the JSON file.
 
     Returns:
          eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
@@ -32,7 +32,7 @@ def write_dataframe_to_csv(df, output_file):
         output_file (str): The path to the output CSV file.
 
     Returns:
-        None
+        (None): function returns nothing
     """
     print(f'Saving to CSV file {output_file}')
     df.to_csv(output_file, index=False)
@@ -83,7 +83,7 @@ def plot_cumulative_time_in_space(df, graph_file):
         graph_file (str): The path to the output graph file.
 
     Returns:
-        None
+        (None): function returns nothing
     """
     print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
     df = add_duration_hours_variable(df)
@@ -116,7 +116,7 @@ def calculate_crew_size(crew):
         crew (str): The text entry in the crew column containing a list of crew member names
 
     Returns:
-        int: The crew size
+        (int): The crew size
     """
     if crew.split() == []:
         return None
